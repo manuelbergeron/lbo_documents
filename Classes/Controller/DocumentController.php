d3ed78f254ba5f615efa0cdeeb06dfabe202fb6b
Manuel Bergeron <manuel.bergeron@libeo.com>
2014-09-10 10:59:10 -0400

    modification et ajout d'icones

--- Classes/Controller/DocumentController.php
+++ Classes/Controller/DocumentController.php
@@ -46,11 +46,30 @@ class DocumentController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControl
 	 * @return void
 	 */
 	public function listAction() {
+        $direction = \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING;
+
+        if ($this->settings["OrderDirection"] == '2') {
+            $direction = \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_DESCENDING;
+        }
+
+        switch ($this->settings["OrderBy"]) {
+            case '2' :
+                $this->documentRepository->setDefaultOrderings(array( 'crdate' => $direction));
+                break;
+            case '1':
+            default:
+                $this->documentRepository->setDefaultOrderings(array( 'uid' => $direction));
+                break;
+        }
+
+
 		$documents = $this->documentRepository->findAll()->toArray();
         foreach($documents as $i => $document) {
             $documents[$i] = $this->evaluateUrl($document);
         }
 
+        $this->replaceIconPath();
+        $this->view->assign('settings', $this->settings);
 		$this->view->assign('documents', $documents);
 	}
 
@@ -82,20 +101,54 @@ class DocumentController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControl
         return $document;
     }
 
+    /**
+     * Replace the path of the icons in the settings
+     */
+    private function replaceIconPath() {
+        if (!isset($this->settings['type'])) {
+            return;
+        }
+
+        foreach($this->settings['type'] as $i => $type) {
+            if (!isset($type['icon'])) {
+                continue;
+            }
+
+            if (substr($this->settings['type'][$i]['icon'], 0, 4) === 'EXT:') {
+                // Get rid of 'EXT:'
+                $image = substr($this->settings['type'][$i]['icon'], 4);
+                list($ext, $path) = explode('/', $image, 2);
+                $extRelPath = substr( \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($ext), strlen(PATH_site));
+                $image = $extRelPath . $path;
+                $this->settings['type'][$i]['icon'] = $image;
+            }
+        }
+    }
+
+    /**
+     * Format the file size with the good unit for the language
+     * @param $bytes
+     * @return string
+     */
     private function formatSizeUnits($bytes)
     {
+        $gb =\TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate("tx_lbodocuments_domain_model_document.filesize.gb", "lbo_documents");
+        $mb =\TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate("tx_lbodocuments_domain_model_document.filesize.mb", "lbo_documents");
+        $kb =\TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate("tx_lbodocuments_domain_model_document.filesize.kb", "lbo_documents");
+        $bt =\TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate("tx_lbodocuments_domain_model_document.filesize.byte", "lbo_documents");
+        $bts =\TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate("tx_lbodocuments_domain_model_document.filesize.bytes", "lbo_documents");
         if ($bytes >= 1073741824) {
-            $bytes = number_format($bytes / 1073741824, 2) . ' Gb';
+            $bytes = number_format($bytes / 1073741824, 2) . ' ' . $gb;
         } elseif ($bytes >= 1048576) {
-            $bytes = number_format($bytes / 1048576, 2) . ' Mb';
+            $bytes = number_format($bytes / 1048576, 2) . ' ' . $mb;
         } elseif ($bytes >= 1024) {
-            $bytes = number_format($bytes / 1024, 2) . ' Kb';
+            $bytes = number_format($bytes / 1024, 2) . ' ' . $kb;
         } elseif ($bytes > 1) {
-            $bytes = $bytes . ' bits';
+            $bytes = $bytes . ' ' .$bts;
         } elseif ($bytes == 1) {
-            $bytes = $bytes . ' bit';
+            $bytes = $bytes . ' ' . $bt;
         } else {
-            $bytes = '0 bytes';
+            $bytes = '0 ' . $bt;
         }
 
         return $bytes;
